bed5c49197c3c449a1651a61de6293a389d143e5
Alex Milroy <amilroy@gmail.com>
2020-10-07 18:14:27 -0700

    psuedo coded the application

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //================================================================
 //Requirements
 //================================================================
-var express = require("Express");
-var app = express();
-var PORT = 3000;
+let express = require("Express");
+let app = express();
+let PORT = 3000;
+//This lets us call different pages
+let path = require("path")
 
 //================================================================
 //Data Handlers
@@ -11,9 +13,20 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//TODO: Delete this after initial structure is setup
 app.get("/", function(req, res) {
     res.send('Note taking API is working')
 });
+
+//TODO: Create the get route that will return index.html file using GET*
+
+//TODO: Create the get route for the notes.html file
+
+//TODO: GET: Create the API route to GET /api/notes from the db.json file, and return saved notes as JSON
+
+//TODO: POST: Create API route for api/notes that should recieve a new note to save on teh request body, add it to db.json, and return the new note to the client
+
+//TODO: DELETE: api/notes/:id should recieve a query parameter with the note ID to be deleted. HINT: Read all notes from the file, remove the note with the correct ID, and then rewrite the db.json
 
 //================================================================
 //Listeners
