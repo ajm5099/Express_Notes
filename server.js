7122199d46df92360205fb2661e3de40bb613930
Alex Milroy <amilroy@gmail.com>
2020-10-08 21:19:54 -0700

    working on putting data on screen

--- server.js
+++ server.js
@@ -4,8 +4,9 @@
 const express = require("Express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require("fs");
 //This lets us call different pages
-const path = require("path")
+const path = require("path");
 
 //================================================================
 //Data Handlers and variables
@@ -14,32 +15,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-let notesData = [
-    {
-        title:"important new note",
-        text:"this is the body of the note we wish to take",
-        id: 1
-    }
-];
+// GET: Create the API route to GET /api/notes from the db.json file, and return saved notes as JSON
+let noteData = fs.readFileSync(path.join(__dirname,"db.json"),"utf8");
+noteData = JSON.parse(noteData);
+console.log(noteData);
+
+let noteId=0;
 
 
 //================================================================
 //Get and Post activities
 //================================================================
 
-//TODO: Create the get route that will return index.html file using GET*
+// Create the get route that will return index.html file using GET*
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname,"index.html"))
 });
 
-//TODO: Create the get route for the notes.html file
+// Create the get route for the notes.html file
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname,"notes.html"));
 });
 
-//TODO: GET: Create the API route to GET /api/notes from the db.json file, and return saved notes as JSON
+
 
 //TODO: POST: Create API route for api/notes that should recieve a new note to save on the request body, add it to db.json, and return the new note to the client
+app.post("/notes",function(req,res){
+    const newNoteObj= {
+        title:req.body.title,
+        text: req.body.text,
+        id: noteId+1
+    }
+    noteId++;
+    fs.writeFileSync(path.join(__dirname,"/db.json"),JSON.stringify(newNoteObj,null,2));
+})
 
 //TODO: DELETE: api/notes/:id should recieve a query parameter with the note ID to be deleted. HINT: Read all notes from the file, remove the note with the correct ID, and then rewrite the db.json
 
